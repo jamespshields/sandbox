b860431356c4e4762bc3d3ce39fcc5b896cc841d
James Shields <jimmyshields@gmail.com>
2025-10-02 23:16:23 -0500

    remove overzealous validation

--- src/utils.rs
+++ src/utils.rs
@@ -28,30 +28,5 @@ pub fn validate_container_name(name: &str) -> Option<String> {
 }
 
 pub fn validate_claude_args(args: &[&str]) -> Result<Vec<String>, String> {
-    let mut validated_args = Vec::new();
-
-    // Regex to check for potentially dangerous patterns
-    let dangerous_patterns = [
-        r"[;&|`$(){}[\]<>]",  // Shell metacharacters
-        r"^\s*-",             // Suspicious flags starting with dash
-    ];
-
-    for arg in args {
-        // Check for dangerous patterns
-        for pattern in &dangerous_patterns {
-            let regex = Regex::new(pattern).unwrap();
-            if regex.is_match(arg) {
-                return Err(format!("Argument contains potentially dangerous characters: {}", arg));
-            }
-        }
-
-        // Check for maximum argument length
-        if arg.len() > 1000 {
-            return Err("Argument too long".to_string());
-        }
-
-        validated_args.push(arg.to_string());
-    }
-
-    Ok(validated_args)
+    Ok(args.iter().map(|s| s.to_string()).collect())
 }
